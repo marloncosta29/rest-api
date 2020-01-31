b482273632df8dffcebca097ff4b0c2bfbd9ab41
marloncosta29 <marlon.29@outlook.com>
2020-01-30 22:59:13 -0300

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import rotaProdutos from './routes/produtos'
 const app = express()
+import morgan from 'morgan'
 
+app.use(morgan('dev'))
 app.use('/produtos', rotaProdutos)
-
-app.use((req, res, next) => {
-  res.status(200).send({ mensagem: 'ok uhuuuuu' })
+app.use((req, res, next)=>{
+  const erro = new Error("NOT FOUND")
+  erro.status(404);
+  next(erro)
 })
-
+app.use((erro, req, res, next)=>{
+  res.status(erro.status || 500);
+  return res.send({
+    erro: {
+      mensagem: erro.mensage
+    }
+  })
+})
 export default app
--- src/routes/produtos.ts
+++ src/routes/produtos.ts
@@ -7,6 +7,14 @@ router
       mensagem: 'usando o get da rota de prods'
     })
   })
+  .get('/:id_produto', (req, res, next)=>{
+    const id = req.params.id_produto;
+
+    res.status(200).send({
+      mensagem: `GET no produto ${id}`
+    })
+
+  })
   .post('/', (req, res, next) => {
     res.status(201).send({
       mensagem: 'usando o post da rota de prods'
